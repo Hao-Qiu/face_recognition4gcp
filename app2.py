71c479a2769958096cddac50917146e9ba689711
Xing Hong <774345882@qq.com>
2020-07-30 03:56:08 +0800

    na

--- app2.py
+++ app2.py
@@ -1,7 +1,4 @@
 import os
-
-from app import app,request
-
 import face_recognition
 from flask import Flask, jsonify, request, redirect
 
@@ -30,10 +27,23 @@ def upload_image():
 
         basedir = os.path.abspath(os.path.dirname(__file__))  # 定义一个根目录 用于保存图片用
 
+        # def editorData():
+        #     imgName = file.filename
+        #     # 定义一个图片存放的位置 存放在static下面
+        #     path = basedir + "/"
+        #     # 图片path和名称组成图片的保存路径
+        #     file_path = path + imgName
+        #     # 保存图片
+        #     file.save(file_path)
+        #     # 这个是图片的访问路径，需返回前端（可有可无）
+        #     url = '/static/img/' + imgName
+        #     return url
+        #     # 返回图片路径 到前端
+
         def editorData():
             imgName = file.filename
             # 定义一个图片存放的位置 存放在static下面
-            path = basedir + "/static/img/"
+            path = basedir + "/"
             # 图片path和名称组成图片的保存路径
             file_path = path + imgName
             # 保存图片
@@ -42,6 +52,9 @@ def upload_image():
             url = '/static/img/' + imgName
             return url
             # 返回图片路径 到前端
+
+        return editorData()
+
 
     # 图片上传失败，输出以下html代码
     return '''
