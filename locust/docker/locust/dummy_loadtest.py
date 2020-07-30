4d2139e0216271c54c67b7d13007c2609d2f6622
Xing Hong <774345882@qq.com>
2020-07-31 00:04:35 +0800

    locust deployment需要添加img地址和测试网址

--- locust/docker/locust/dummy_loadtest.py
+++ locust/docker/locust/dummy_loadtest.py
@@ -1,1 +1,22 @@
 # 还不知道怎么写
+import uuid
+from locust import HttpLocust, TaskSet, task
+
+
+class MetricsTaskSet(TaskSet):
+
+    def on_start(self):
+        self._deviceid = str(uuid.uuid4())
+
+    @task(1)
+    def index(self):
+        self.client.get("/")
+
+    @task(2)
+    def post_metrics(self):
+        self.client.post(
+            "/", {"file": self.file})
+#我真的在乱写 根本不知道数据是怎么传的
+
+class MetricsLocust(HttpLocust):
+    task_set = MetricsTaskSet
